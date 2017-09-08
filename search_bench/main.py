6b5f84f1306ef2cc336015600ed48924f58f613d
Kyle McCrohan <kmccrohan@zagmail.gonzaga.edu>
2017-09-07 20:15:44 -0700

    Added a basic random search algorithm

--- search_bench/main.py
+++ search_bench/main.py
@@ -1,25 +1,25 @@
 from results import parse_results
 from run_test import run_test
 
-print(parse_results("""
-
-wejfoiwjoe fjiwe
-weoifjwe oijfoiew
-weojfoiwejfoiwej
-weoifjwoiejfoiwe
-woiejfoiwjeofijweoifjo
-
-__RESULTS__
-101
-1000
-ewoifjwioe
-ofeijwoiejfiow
-oiwejfoijweof
-oiwfjeoifjwo
-ofijweofjow
-lwjefoiwej
-__END_RESULTS__
-
-foiwejfoiwejoif
-fwjeiofjweiojf
-"""))
+# print(parse_results("""
+#
+# wejfoiwjoe fjiwe
+# weoifjwe oijfoiew
+# weojfoiwejfoiwej
+# weoifjwoiejfoiwe
+# woiejfoiwjeofijweoifjo
+#
+# __RESULTS__
+# 101
+# 1000
+# ewoifjwioe
+# ofeijwoiejfiow
+# oiwejfoijweof
+# oiwfjeoifjwo
+# ofijweofjow
+# lwjefoiwej
+# __END_RESULTS__
+#
+# foiwejfoiwejoif
+# fwjeiofjweiojf
+# """))
