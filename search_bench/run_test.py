51b6d519a24483a1f280af3edf86e3aef54468c2
Kyle McCrohan <kmccrohan@zagmail.gonzaga.edu>
2017-09-07 19:47:57 -0700

    fixed where function headers are

--- search_bench/run_test.py
+++ search_bench/run_test.py
@@ -2,6 +2,7 @@ import json
 from subprocess import call
 
 
+def run_tests(test_file):
 """
 Runs a test file and all of its tests.
 
@@ -11,7 +12,6 @@ Params:
 Returns:
     ???
 """
-def run_tests(test_file):
     tests = load_json(test_file)
 
     dataset = test["dataset_file"]
@@ -24,13 +24,15 @@ def run_tests(test_file):
     print metrics
 
 
+def load_json(jfile):
 """
 Loads json from a file
 """
-def load_json(jfile):
     with open(jfile) as json_file:
         return json.load(json_file)
 
+
+def test_search(dataset_file, command, test):
 """
 Tests a specific search_text on the algorithm
 
@@ -42,7 +44,6 @@ Params:
 Returns:
     Result hueristics
 """
-def test_search(dataset_file, command, test):
     search_text = test["search_text"]
     n_results = len(test["results"])
     call([command, dataset_file, search_text, n_results])
