04aa3b4ce3b7409645a887659898dd4a87145b76
Ethan Mahintorabi <emahintorabi@asperasoft.com>
2017-09-07 20:24:14 -0700

    fixing merge conflict

--- search_bench/main.py
+++ search_bench/main.py
@@ -1,32 +1,6 @@
 from results import parse_results, score
 from run_test import run_test
 
-<<<<<<< HEAD
-print(parse_results("""
-
-wejfoiwjoe fjiwe
-weoifjwe oijfoiew
-weojfoiwejfoiwej
-weoifjwoiejfoiwe
-woiejfoiwjeofijweoifjo
-
-__RESULTS__
-101
-1000
-ewoifjwioe
-ofeijwoiejfiow
-oiwejfoijweof
-oiwfjeoifjwo
-ofijweofjow
-lwjefoiwej
-__END_RESULTS__
-
-foiwejfoiwejoif
-fwjeiofjweiojf
-"""))
-
-print(score(["test", "woo"], ["test", "shoo"]))
-=======
 # print(parse_results("""
 #
 # wejfoiwjoe fjiwe
@@ -49,4 +23,3 @@ print(score(["test", "woo"], ["test", "shoo"]))
 # foiwejfoiwejoif
 # fwjeiofjweiojf
 # """))
->>>>>>> 6b5f84f1306ef2cc336015600ed48924f58f613d
