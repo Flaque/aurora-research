7edf6daa404c0c6e54b89fc392986496e7babead
Scott <srein@zagmail.gonzaga.edu>
2017-09-12 18:38:33 -0700

    get_args function complete

--- search/candidates/fuzzball/index.js
+++ search/candidates/fuzzball/index.js
@@ -1,7 +1,12 @@
 const Fuse = require('fuse.js');
 
-function get_args(args) {
-
+function get_args(argv) {
+argsObj={
+  "dataset_file":argv[2],
+  "search_text":argv[3],
+  "n_results":Number(argv[4])
+  }
+  return argsObj;
 }
 
 function populate_fuse(items) {
@@ -9,7 +14,8 @@ function populate_fuse(items) {
 }
 
 function search_fuse(fuse) {
-  
+
 }
 
 console.log('Hello world');
+console.log(get_args(process.argv));
